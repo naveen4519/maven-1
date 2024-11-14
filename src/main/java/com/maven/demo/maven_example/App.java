b6c7bf940902bb9228eb177c0a915086afb1378e
Naveen <nakirikantinaveen19@gmail.com>
2024-11-14 15:39:59 +0530

    First Commit

---
+++ src/main/java/com/maven/demo/maven_example/App.java
@@ -0,0 +1,10 @@
+package com.maven.demo.maven_example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Naveen 22bd1a053b");
+    }
+}
