c4849dd851b4d85d45da02c7cf02c56e0dc65988
naveen4519 <157727466+naveen4519@users.noreply.github.com>
2025-01-17 13:34:44 +0530

    Update App.java

--- src/main/java/com/maven/demo/maven_example/App.java
+++ src/main/java/com/maven/demo/maven_example/App.java
@@ -1,10 +1,26 @@
-package com.maven.demo.maven_example;
+package com.app.demokmit.SEexternal99;
 
 /**
  * Hello world!
+ *
  */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Naveen 22bd1a053b");
-    }
+public class App 
+{
+	public static int add(int a, int b) {
+		return a+b;
+	}
+	public static int subtract(int a, int b) {
+		return a-b;
+	}
+	public static int multiply(int a, int b) {
+		return a*b;
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Add 2 and 3 : " + add(2,3));
+        System.out.println("Subtract 7 and 5 : " + subtract(7,5));
+        System.out.println("Multiply 10 and 2 : " + multiply(10, 2));
+        }
 }
